e77e785235fc95b67587184444aa341cf7a4f3fd
Marcel Dejean <marcel@dejean.nyc>
2023-03-15 00:21:54 -0400

    fetch upcoming trains for a stop from goodservice api

---
+++ config.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+
+api_key = ''
+
+station = '234'
+
+direction = 'north'
+
+goodservice_url = 'https://www.goodservice.io/api/stops/' + station
+
+DISPLAY_WIDTH = 192
+DISPLAY_HEIGHT = 32
---
+++ get_data.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+import requests
+import json
+import pickle
+from config import *
+
+headers = {'x-application': 'signpi', 'x-api-key': api_key}
+
+with requests.get(goodservice_url) as req:
+    body = req.json()
+
+    #    now = body['timestamp']
+
+    # TODO: both directions, filter routes
+    trips = body['upcoming_trips'][direction]
+
+    #    for n in range(min(4, len(trips))):
+    #        t = trips[n]
+    #        eta_min = round((t['estimated_current_stop_arrival_time'] - now) / 60)
+
+    #        print(f"{n}. {t['route_id']} {t['destination_stop']} {eta_min} min")
+
+    with open('trips.json', 'w') as f:
+        json.dump(trips[:min(4, len(trips))], f)
