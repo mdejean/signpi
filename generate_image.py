2297921b124fd3867b5abbe0ab2cc1bc39547f7d
Marcel Dejean <marcel@dejean.nyc>
2023-03-15 00:23:23 -0400

    create sign file from real time data

--- generate_image.py
+++ generate_image.py
@@ -1,97 +1,40 @@
 #!/usr/bin/env python3
 """
-Update the sign
+Generate COLOR_01.PRG
 """
 
-from PIL import Image, ImageDraw, ImageFont
+from config import *
+import layout
 
-DISPLAY_WIDTH = 192
-DISPLAY_HEIGHT = 32
+from PIL import Image
+import json
 
 
 def main():
+    with open('trips.json') as f:
+        trips = json.load(f)
+
     img = Image.new('RGB', (DISPLAY_WIDTH, DISPLAY_HEIGHT))
-    mania = ImageFont.truetype('mania.ttf', 14)
-    pokemon = ImageFont.truetype('Pokemon X and Y.ttf', 11)
-    fifteen = ImageFont.truetype('15x5.ttf', 16)
-    pm = ImageFont.truetype('pixelmix_bold.ttf', 8)
-    draw = ImageDraw.Draw(img)
 
-    LINE_HEIGHT = 16
+    layout.draw(img, trips)
 
-    line1_mid = LINE_HEIGHT / 2
-    line2_mid = DISPLAY_HEIGHT - LINE_HEIGHT / 2
+    # for debugging
+    img.show()
 
-    BULLET_SIZE = 14
-    ORDER_WIDTH = 8
-
-    draw.text((0, 0),
-              "1.",
-              font=fifteen,
-              anchor='lt',
-              fill=(255, 255, 255))
-    draw.text((0, DISPLAY_HEIGHT),
-              "3.",
-              font=fifteen,
-              anchor='lb',
-              fill=(255, 255, 255))
-
-    draw.ellipse([(ORDER_WIDTH + 0, 0), (ORDER_WIDTH + BULLET_SIZE, BULLET_SIZE)],
-             fill=(0, 255, 0),
-             width=2)
-    draw.text((ORDER_WIDTH + BULLET_SIZE / 2 + 1, BULLET_SIZE / 2),
-              "4",
-              font=pm,
-              anchor='mm',
-              fill=(0, 0, 0))
-    draw.text((ORDER_WIDTH + BULLET_SIZE + 4, line1_mid),
-              "Brooklyn Bridge - City Hall",
-              font=pokemon,
-              anchor='lm',
-              fill=(255, 255, 255))
-    draw.ellipse([(ORDER_WIDTH + 0, DISPLAY_HEIGHT - 1 - BULLET_SIZE),
-              (ORDER_WIDTH + BULLET_SIZE, DISPLAY_HEIGHT - 1)],
-             fill=(127, 0, 0),
-             width=2)
-    draw.text((ORDER_WIDTH + BULLET_SIZE / 2 + 1,
-               DISPLAY_HEIGHT - 1 - BULLET_SIZE / 2),
-              "2",
-              font=pm,
-              anchor='mm',
-              fill=(255, 255, 255))
-    draw.text((ORDER_WIDTH + BULLET_SIZE + 4, line2_mid),
-              "Van Cortlandt Park - 242nd Street",
-              font=pokemon,
-              anchor='lm',
-              fill=(255, 255, 255))
-    # draw.text((192, 0),
-    #           "10:47 PM",
-    #           font=mania,
-    #           anchor='rt',
-    #           fill=(255, 255, 255))
-
-    draw.text((DISPLAY_WIDTH, line1_mid),
-              text="3 min",
-              anchor="rm",
-              font=pokemon)
-    draw.text((DISPLAY_WIDTH, line2_mid),
-              text="6 min",
-              anchor="rm",
-              font=pokemon)
-    
     output = None
     with open('template.bin', 'rb') as f:
         output = f.read()
-    
     # output is column-first
-    img = img.transpose(Image.Transpose.TRANSPOSE)
+    img = img.transpose(Image.Transpose.TRANSPOSE if hasattr(
+        Image, 'Transpose') else Image.TRANSPOSE)  #PIL 8.1 change
     output += bytes([
-        (pixel[0] >> 6) | ((pixel[1] >> 6) << 2) | ((pixel[2] >> 6) << 4) 
+        (pixel[0] >> 6) | ((pixel[1] >> 6) << 2) | ((pixel[2] >> 6) << 4)
         for pixel in img.getdata()
     ])
-    
+
     with open('COLOR_01.PRG', 'wb') as f:
         f.write(output)
+
 
 if __name__ == '__main__':
     main()
---
+++ layout.py
@@ -0,0 +1,112 @@
+#!/usr/bin/python
+
+from config import *
+
+import time
+import csv
+from PIL import ImageDraw, ImageFont
+
+LINE_HEIGHT = 16
+
+line_heights = [LINE_HEIGHT / 2, DISPLAY_HEIGHT - LINE_HEIGHT / 2]
+
+BULLET_SIZE = 14
+ORDER_WIDTH = 8
+
+
+def draw(img, trips):
+    # TODO: alternating pages
+    with open('routes.txt', 'r') as f:
+        routes = {r['route_id']: r for r in csv.DictReader(f)}
+
+    with open('stops.txt', 'r') as f:
+        stops = {s['stop_id']: s for s in csv.DictReader(f)}
+
+    now = time.time()
+
+    if len(trips) < 2 or max(t['timestamp'] for t in trips) < now - 60 * 5:
+        #TODO: no data screen
+        return
+    for i in (0, 1):
+        route_id = trips[i]['route_id']
+        stop_id = trips[i]['destination_stop']
+
+        eta_min = round(
+            (trips[i]['estimated_current_stop_arrival_time'] - now) / 60)
+        if eta_min > 99 or trips[i]['is_delayed']:
+            eta = 'Delay'
+        else:
+            eta = f'{eta_min} min'
+
+        draw_trip(img,
+                  line_heights[i],
+                  order=f'{i+1}.',
+                  route=routes[route_id]['route_short_name'],
+                  route_color='#' +
+                  (routes[route_id]['route_color'] or '000000'),
+                  route_text_color='#' +
+                  (routes[route_id]['route_text_color'] or 'ffffff'),
+                  dest=stops[stop_id]['stop_name'],
+                  dest_short='x',
+                  eta=eta)
+
+    # draw.text((192, 0),
+    #           "10:47 PM",
+    #           font=mania,
+    #           anchor='rt',
+    #           fill=(255, 255, 255))
+
+
+def draw_trip(img,
+              y,
+              order,
+              route,
+              route_color,
+              route_text_color,
+              dest,
+              dest_short,
+              eta,
+              eta_fill=(255, 255, 255)):
+    mania = ImageFont.truetype('mania.ttf', 14)
+    pokemon = ImageFont.truetype('Pokemon X and Y.ttf', 11)
+    fifteen = ImageFont.truetype('15x5.ttf', 16)
+    pm = ImageFont.truetype('pixelmix_bold.ttf', 8)
+    draw = ImageDraw.Draw(img)
+
+    LINE_HEIGHT = 16
+
+    line1_mid = LINE_HEIGHT / 2
+    line2_mid = DISPLAY_HEIGHT - LINE_HEIGHT / 2
+
+    BULLET_SIZE = 14
+    ORDER_WIDTH = 8
+
+    draw.text((0, y), order, font=fifteen, anchor='lm', fill=(255, 255, 255))
+    #draw.text((0, line2_mid + 1), # + 1 because this font sucks
+
+    draw.ellipse([(ORDER_WIDTH + 0, y - BULLET_SIZE / 2),
+                  (ORDER_WIDTH + BULLET_SIZE, y + BULLET_SIZE / 2)],
+                 fill=route_color,
+                 width=2)
+    draw.text((ORDER_WIDTH + BULLET_SIZE / 2 + 1, y),
+              text=route,
+              font=pm,
+              anchor='mm',
+              fill=route_text_color)
+
+    dest_x = ORDER_WIDTH + BULLET_SIZE + 4
+    available_space = DISPLAY_WIDTH - dest_x - max(pokemon.getlength('Delay'),
+                                                   pokemon.getlength('99 min'))
+
+    draw.text(
+        (dest_x, y),
+        text=dest_short if pokemon.getlength(dest) > available_space else dest,
+        font=pokemon,
+        anchor='lm',
+        fill=(255, 255, 255))
+
+    draw.text((DISPLAY_WIDTH, y),
+              text=eta,
+              anchor="rm",
+              font=pokemon,
+              fill=eta_fill)
