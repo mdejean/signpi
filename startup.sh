a1fe41654ec607357b11b5c7fbe634a9958efe16
Marcel Dejean <marcel@dejean.nyc>
2023-03-18 09:37:10 -0400

    wait for dhcpd to finish setting up wpa_supplicant before updating

--- startup.sh
+++ startup.sh
@@ -5,4 +5,8 @@ set -e
 losetup -o512 /dev/loop0 /var/local/mass_storage_backing
 mount -o umask=000 -t vfat /dev/loop0 /media/mass_storage_gadget
 
+sleep 5
+
+systemctl restart dhcpcd
+
 /usr/local/lib/signpi/load_config.py
