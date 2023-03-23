f0c53ddf046d514a02d73c6aae7cac24b792f41e
Marcel Dejean <marcel@dejean.nyc>
2023-03-23 11:17:43 -0400

    cleanup

--- layout.py
+++ layout.py
@@ -13,25 +13,30 @@ line_heights = [LINE_HEIGHT / 2, DISPLAY_HEIGHT - LINE_HEIGHT / 2 + 1]
 BULLET_SIZE = 14
 ORDER_WIDTH = 8
 
+with open('routes.txt', 'r') as f:
+    routes = {r['route_id']: r for r in csv.DictReader(f)}
+
+with open('stops.txt', 'r') as f:
+    stops = {s['stop_id']: s for s in csv.DictReader(f)}
+
+mania = ImageFont.truetype('mania.ttf', 14)
+pokemon = ImageFont.truetype('Pokemon X and Y.ttf', 11)
+fifteen = ImageFont.truetype('15x5.ttf', 16)
+pm = ImageFont.truetype('pixelmix_bold.ttf', 8)
+
 
 def draw(img, trips):
     # TODO: alternating pages
-    with open('routes.txt', 'r') as f:
-        routes = {r['route_id']: r for r in csv.DictReader(f)}
-
-    with open('stops.txt', 'r') as f:
-        stops = {s['stop_id']: s for s in csv.DictReader(f)}
 
     now = time.time()
     draw = ImageDraw.Draw(img)
 
     if len(trips) < 2 or max(t['timestamp'] for t in trips) < now - 60 * 5:
         draw.text((0, 0), "No data", anchor='lt')
-        draw.text((0,0), "No data", anchor='lt')
         #TODO: better no data screen
         return
     for i in (0, 1):
-        route_id = trips[i]['route_id']
+        route = routes[trips[i]['route_id']]
         stop_id = trips[i]['destination_stop']
 
         eta_min = round(
@@ -40,24 +45,24 @@ def draw(img, trips):
             eta = 'Delay'
         else:
             eta = f'{eta_min} min'
-        if len(routes[route_id]['route_color']) == 6:
-            route_color = ImageColor.getrgb('#' +
-                                            routes[route_id]['route_color'])
+        if len(route['route_color']) == 6:
+            route_color = ImageColor.getrgb(f"#{route['route_color']}")
         else:
             route_color = ImageColor.getrgb('black')
 
-        if len(routes[route_id]['route_text_color']) == 6:
+        if len(route['route_text_color']) == 6:
             route_text_color = ImageColor.getrgb(
-                '#' + routes[route_id]['route_text_color'])
+                f"#{route['route_text_color']}")
         else:
-            route_text_color = ImageColor.getrgb(
-                'black') if sum(route_color) / 3 > 127 else ImageColor.getrgb(
-                    'white')
+            if sum(route_color) / 3 > 127:
+                route_text_color = ImageColor.getrgb('black')
+            else:
+                route_text_color = ImageColor.getrgb('white')
 
         draw_trip(draw,
                   line_heights[i],
                   order=f'{i+1}.',
-                  route=routes[route_id]['route_short_name'],
+                  route=route['route_short_name'],
                   route_color=route_color,
                   route_text_color=route_text_color,
                   dest=stops[stop_id]['stop_name'],
@@ -81,15 +86,19 @@ def draw_trip(draw,
               dest_short,
               eta,
               eta_fill=(255, 255, 255)):
-    mania = ImageFont.truetype('mania.ttf', 14)
-    pokemon = ImageFont.truetype('Pokemon X and Y.ttf', 11)
-    fifteen = ImageFont.truetype('15x5.ttf', 16)
-    pm = ImageFont.truetype('pixelmix_bold.ttf', 8)
+
+    order_font = fifteen
+    bullet_font = pm
+    main_font = pokemon
 
     BULLET_SIZE = 14
     ORDER_WIDTH = 8
 
-    draw.text((0, y), order, font=fifteen, anchor='lm', fill=(255, 255, 255))
+    draw.text((0, y),
+              order,
+              font=order_font,
+              anchor='lm',
+              fill=(255, 255, 255))
 
     draw.ellipse([(ORDER_WIDTH + 0, y - BULLET_SIZE / 2 - 1),
                   (ORDER_WIDTH + BULLET_SIZE, y + BULLET_SIZE / 2 - 1)],
@@ -97,23 +106,23 @@ def draw_trip(draw,
                  width=2)
     draw.text((ORDER_WIDTH + BULLET_SIZE / 2 + 1, y - 1),
               text=route,
-              font=pm,
+              font=bullet_font,
               anchor='mm',
               fill=route_text_color)
 
     dest_x = ORDER_WIDTH + BULLET_SIZE + 4
-    available_space = DISPLAY_WIDTH - dest_x - max(pokemon.getlength('Delay'),
-                                                   pokemon.getlength('99 min'))
+    available_space = DISPLAY_WIDTH - dest_x - max(
+        main_font.getlength('Delay'), main_font.getlength('99 min'))
 
-    draw.text(
-        (dest_x, y),
-        text=dest_short if pokemon.getlength(dest) > available_space else dest,
-        font=pokemon,
-        anchor='lm',
-        fill=(255, 255, 255))
+    draw.text((dest_x, y),
+              text=dest_short
+              if main_font.getlength(dest) > available_space else dest,
+              font=main_font,
+              anchor='lm',
+              fill=(255, 255, 255))
 
     draw.text((DISPLAY_WIDTH, y),
               text=eta,
               anchor="rm",
-              font=pokemon,
+              font=main_font,
               fill=eta_fill)
